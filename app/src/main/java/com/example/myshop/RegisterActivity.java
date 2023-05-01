e65937301d237e5ab1d15c27101a514737dbb3c8
GorBada1yan <gorb965@gmail.com>
2023-05-01 20:29:09 +0400

    Connecting to FireBase(Registration Activity)

--- app/src/main/java/com/example/myshop/RegisterActivity.java
+++ app/src/main/java/com/example/myshop/RegisterActivity.java
@@ -1,15 +1,33 @@
 package com.example.myshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Button registerBtn;
     private EditText usernameInput, phoneInput, passwordInput;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,87 @@ public class RegisterActivity extends AppCompatActivity {
         usernameInput = (EditText) findViewById(R.id.register_username_input);
         passwordInput = (EditText) findViewById(R.id.register_password_input);
         phoneInput = (EditText) findViewById(R.id.register_phone_input);
+        loadingBar = new ProgressDialog(this);
 
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateAccount();
+            }
+        });
+
+    }
+
+    private void CreateAccount() {
+        String username = usernameInput.getText().toString();
+        String phone = phoneInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        if (TextUtils.isEmpty(username)){
+            Toast.makeText(this, "Введите Имя", Toast.LENGTH_SHORT).show();
+        }else if (TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "Введите Номер Телефона", Toast.LENGTH_SHORT).show();
+        }else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Введите Пароль", Toast.LENGTH_SHORT).show();
+        }else {
+            loadingBar.setTitle("Создание аккаунта");
+            loadingBar.setMessage("Пожалуйста подаждите");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            //проверка регистрации
+
+            ValidatePhone(username, phone, password);
+             
+
+        }
+
+    }
+
+    private void ValidatePhone(String username, String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!(snapshot.child("Users").child(phone).exists())){
+                    HashMap<String, Object> userDataMap = new HashMap<>();
+                    userDataMap.put("Phone", phone);
+                    userDataMap.put("UserName", username);
+                    userDataMap.put("Password", password);
+
+                    RootRef.child("Users").child(phone).updateChildren(userDataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    loadingBar.dismiss();
+                                    Toast.makeText(RegisterActivity.this, "Вы были успешно зарегистрированы", Toast.LENGTH_LONG).show();
+                                    Intent loginIntent = new Intent(RegisterActivity.this , LoginActivity.class);
+                                    startActivity(loginIntent);
+                                }else {
+                                    loadingBar.dismiss();
+                                    Toast.makeText(RegisterActivity.this, " Ошибка ", Toast.LENGTH_SHORT).show();
+                                }
+                                }
+                            });
+
+
+                }
+
+                else {
+                    loadingBar.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Пользователь с таким номером уже существует", Toast.LENGTH_LONG).show();
+
+                    Intent loginIntent = new Intent(RegisterActivity.this , LoginActivity.class);
+                    startActivity(loginIntent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
