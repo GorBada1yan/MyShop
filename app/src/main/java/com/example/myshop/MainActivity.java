814822aea2e532e2807b69632ea19bff2954065b
GorBada1yan <gorb965@gmail.com>
2023-05-01 22:39:19 +0400

    1213

--- app/src/main/java/com/example/myshop/MainActivity.java
+++ app/src/main/java/com/example/myshop/MainActivity.java
@@ -7,8 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     private Button joinButton, loginButton;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
 
     @Override
