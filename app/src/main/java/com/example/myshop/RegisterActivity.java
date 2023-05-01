875a15eea6219cd65319bb4fc63d5ff8beebafa5
GorBada1yan <gorb965@gmail.com>
2023-05-02 00:36:43 +0400

    Registration by using email and password

--- app/src/main/java/com/example/myshop/RegisterActivity.java
+++ app/src/main/java/com/example/myshop/RegisterActivity.java
@@ -1,9 +1,6 @@
 package com.example.myshop;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,112 +9,73 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private Button registerBtn;
-    private EditText usernameInput, phoneInput, passwordInput;
-    private ProgressDialog loadingBar;
+    private EditText mUsername;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mRegisterButton;
 
+    private FirebaseAuth mAuth;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        registerBtn= (Button)findViewById(R.id.register_btn);
-        usernameInput = (EditText) findViewById(R.id.register_username_input);
-        passwordInput = (EditText) findViewById(R.id.register_password_input);
-        phoneInput = (EditText) findViewById(R.id.register_phone_input);
-        loadingBar = new ProgressDialog(this);
 
+        mAuth = FirebaseAuth.getInstance();
 
-        registerBtn.setOnClickListener(new View.OnClickListener() {
+        mUsername = findViewById(R.id.register_username_input);
+        mEmail = findViewById(R.id.register_email_input);
+        mPassword = findViewById(R.id.register_password_input);
+        mRegisterButton = findViewById(R.id.register_btn);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                CreateAccount();
-            }
-        });
+            public void onClick(View v) {
+                String username = mUsername.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
-    }
-
-    private void CreateAccount() {
-        String username = usernameInput.getText().toString();
-        String phone = phoneInput.getText().toString();
-        String password = passwordInput.getText().toString();
-
-        if (TextUtils.isEmpty(username)){
-            Toast.makeText(this, "Введите Имя", Toast.LENGTH_SHORT).show();
-        }else if (TextUtils.isEmpty(phone)){
-            Toast.makeText(this, "Введите Номер Телефона", Toast.LENGTH_SHORT).show();
-        }else if (TextUtils.isEmpty(password)){
-            Toast.makeText(this, "Введите Пароль", Toast.LENGTH_SHORT).show();
-        }else {
-            loadingBar.setTitle("Создание аккаунта");
-            loadingBar.setMessage("Пожалуйста подаждите");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
-            //проверка регистрации
-
-            ValidatePhone(username, phone, password);
-             
-
-        }
-
-    }
-
-    private void ValidatePhone(String username, String phone, String password) {
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
-        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (!(snapshot.child("Users").child(phone).exists())){
-                    HashMap<String, Object> userDataMap = new HashMap<>();
-                    userDataMap.put("Phone", phone);
-                    userDataMap.put("UserName", username);
-                    userDataMap.put("Password", password);
-
-                    RootRef.child("Users").child(phone).updateChildren(userDataMap)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()){
-                                    loadingBar.dismiss();
-                                    Toast.makeText(RegisterActivity.this, "Вы были успешно зарегистрированы", Toast.LENGTH_LONG).show();
-                                    Intent loginIntent = new Intent(RegisterActivity.this , LoginActivity.class);
-                                    startActivity(loginIntent);
-                                }else {
-                                    loadingBar.dismiss();
-                                    Toast.makeText(RegisterActivity.this, " Ошибка ", Toast.LENGTH_SHORT).show();
-                                }
-                                }
-                            });
-
-
+                if (TextUtils.isEmpty(username)) {
+                    mUsername.setError("Имя пользователя обязательно");
+                    return;
                 }
 
-                else {
-                    loadingBar.dismiss();
-                    Toast.makeText(RegisterActivity.this, "Пользователь с таким номером уже существует", Toast.LENGTH_LONG).show();
-
-                    Intent loginIntent = new Intent(RegisterActivity.this , LoginActivity.class);
-                    startActivity(loginIntent);
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email обязателен");
+                    return;
                 }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Пароль обязателен");
+                    return;
+                }
 
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Регистрация прошла успешно", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "Регистрация не удалась: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
 }
+
