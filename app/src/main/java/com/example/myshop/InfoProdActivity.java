c1f84de265c75d6f79c4c8690ac3c3c104906b11
GorBada1yan <gorb965@gmail.com>
2023-05-12 15:45:46 +0400

    InfoProductActivity

---
+++ app/src/main/java/com/example/myshop/InfoProdActivity.java
@@ -0,0 +1,41 @@
+package com.example.myshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class InfoProdActivity extends AppCompatActivity {
+
+    private TextView info_back, info_add_favorite, info_description, info_name ,info_price,info_contacts;
+    private ImageView info_photo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_prod);
+        info_back = findViewById(R.id.info_back);
+        info_add_favorite = findViewById(R.id.info_add_favorite);
+        info_description = findViewById(R.id.info_description);
+        info_name = findViewById(R.id.info_name);
+        info_price = findViewById(R.id.info_price);
+        info_contacts = findViewById(R.id.info_contacts);
+        info_photo = findViewById(R.id.info_photo)  ;
+
+        info_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent infoback = new Intent(InfoProdActivity.this, HomeActivity.class);
+                startActivity(infoback);
+            }
+        });
+
+        // TODO info_add_favorite button functional
+
+
+
+    }
+}
--- app/src/main/java/com/example/myshop/ViewHolder/ProductViewHolder.java
+++ app/src/main/java/com/example/myshop/ViewHolder/ProductViewHolder.java
@@ -1,40 +1,49 @@
 package com.example.myshop.ViewHolder;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myshop.InfoProdActivity;
 import com.example.myshop.Interface.ItemClickListner;
 import com.example.myshop.R;
 
-public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
-{
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     public TextView txtProductName, txtProductDescription, txtProductPrice;
     public ImageView imageView;
     public ItemClickListner listner;
 
-
-    public ProductViewHolder(View itemView)
-    {
+    public ProductViewHolder(View itemView) {
         super(itemView);
-
 
         imageView = itemView.findViewById(R.id.product_image);
         txtProductName = itemView.findViewById(R.id.product_name);
         txtProductDescription = itemView.findViewById(R.id.product_description);
         txtProductPrice = itemView.findViewById(R.id.product_price);
+
+
+        itemView.setOnClickListener(this);
+        imageView.setOnClickListener(this);
     }
 
-    public void setItemClickListner(ItemClickListner listner)
-    {
+    public void setItemClickListner(ItemClickListner listner) {
         this.listner = listner;
     }
 
     @Override
-    public void onClick(View view)
-    {
-        listner.onClick(view, getAdapterPosition(), false);
+    public void onClick(View view) {
+        if (view.getId() == imageView.getId()) {
+
+            Intent intent = new Intent(view.getContext(), InfoProdActivity.class);
+
+
+            view.getContext().startActivity(intent);
+        } else {
+
+            listner.onClick(view, getAdapterPosition(), false);
+        }
     }
 }
